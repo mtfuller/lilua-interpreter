c4335908c79bc3d6d1047a60f1bcd11a0efb8af2
Thomas Fuller <Thomas Fuller>
2017-02-15 23:21:57 -0500

    Made skeleton of Scanner class & definitions

---
+++ lilua-compiler-src/Scanner/lilua_symbol.h
@@ -0,0 +1,28 @@
+// =============================================================================
+// Author(s): Thomas Fuller
+// Project:   Module 3 - 1st Deliverable
+// File:      lilua_symbol.h
+// =============================================================================
+// Description:
+//
+// =============================================================================
+
+// TOKEN DEFINITIONS:
+#define EOF_TOKEN               -1
+#define UNKNOWN_TOKEN           0
+#define ID                      1
+#define LITERAL_INTEGER         2
+#define ASSIGNMENT_OPERATOR     3
+#define LE_OPERATOR             4
+// ...
+#define DIV_OPERATOR            13
+
+
+// KEYWORDS
+// Needed keywords: null, function, end, if, then, else, while, do, repeat,
+// until, print,
+// ALPHABETICAL ORDER!!!
+#define NULL_KEYWORD            14
+#define FUNCTION_KEYWORD        15
+#define END_KEYWORD             16
+// ...
